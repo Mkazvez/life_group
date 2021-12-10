300fe84724589f8f14d472016795b56334da7ebd
Kirill Mozhaev <mozhaevka2@gmail.com>
2021-12-10 23:47:38 +0500

    Первая загрузка

---
+++ my-app_server/app/models/department.model.js
@@ -0,0 +1,10 @@
+module.exports = (sequelize, Sequelize) => {
+    const Department = sequelize.define("department", {
+      name: {
+        type: Sequelize.STRING
+      }      
+    }
+  );
+    return Department;
+  };
+  
